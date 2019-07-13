78d40025bb085d3a782407bfedbea8aba4df5df4
Jefferson Daniel Castro Aguilar <jedcastroag@unal.edu.co>
2019-07-13 13:44:27 -0500

    database script modified

--- notificaciones-db/SQL_Script.sql
+++ notificaciones-db/SQL_Script.sql
@@ -1,16 +1,29 @@
 USE notificaciones_bd;
 
-DROP TABLE IF EXISTS notificaciones;
-
-    CREATE TABLE IF NOT EXISTS notificaciones
-    (
+CREATE TABLE
+IF NOT EXISTS notificaciones
+(
         id INT
         AUTO_INCREMENT, 
     fecha DATETIME NOT NULL,
     activa BOOLEAN NOT NULL,
-    motivo varchar(150) NOT NULL, 
+    motivo varchar
+(150) NOT NULL, 
     id_usuario INT NOT NULL, 
     id_excursion int not null, 
     PRIMARY KEY
-        (id)
+(id)
 );
+
+CREATE INDEX
+IF NOT EXISTS fecha_index ON notificaciones
+(fecha);
+CREATE INDEX
+IF NOT EXISTS id_usuario_index ON notificaciones
+(id_usuario);
+CREATE INDEX
+IF NOT EXISTS id_excursion_index ON notificaciones
+(id_excursion);
+CREATE INDEX
+IF NOT EXISTS status_index ON notificaciones
+(activa);
